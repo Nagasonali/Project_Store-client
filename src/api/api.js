b64f9a3be54576ada3a34c491cad2719da232a88
Sonali <nagasonali5676@gmail.com>
2024-11-16 10:51:14 +0530

    Day-18

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,68 @@
-
 import axios from 'axios'
+import { getToken } from '../service/auth'
 
 
 const API = 'http://localhost:3000'
 
+
+const axiosInstance = axios.create({
+    baseURL: API,
+})
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = getToken()
+        if (token) {
+            config.headers.Authorization = `${token}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
 // ProductEndPonts
 const getProducts = () => axios.get(`${API}/products/all`)
-const addProduct = (product) => axios.post(`${API}/products/add`, product)
-const editProduct = (product, id) => axios.put(`${API}/products/edit/${id}`, product)
-const deleteProduct = (id) => axios.delete(`${API}/products/delete/${id}`)
+const getProductsCount = () => axiosInstance.get(`/products/count`)
+const addProduct = (product) => axiosInstance.post(`${API}/products/add`, product)
+const editProduct = (product, id) => axiosInstance.put(`${API}/products/edit/${id}`, product)
+const deleteProduct = (id) => axiosInstance.delete(`${API}/products/delete/${id}`)
 
 // OrderEndPonts
-const getOrders = () => axios.get(`${API}/orders/all`)
+const getOrders = () => axiosInstance.get(`${API}/orders/all`)
+const addOrder = (order) => axiosInstance.post('/orders/add', order)
+const getOrdersCount = () => axiosInstance.get(`/orders/count`)
+const deleteOrder = (id) => axiosInstance.delete(`${API}/orders/delete/${id}`)
 
 
 //UserEndPoints
-const getUsers = () => axios.get(`${API}/users/all`)
-
+const getUsers = () => axiosInstance.get(`/users/all`)
+const getUsersCount = () => axiosInstance.get(`/users/count`)
+const addUser = (user) => axiosInstance.post(`/users/add`, user)
+const editUser = (user, id) => axios.put(`${API}/users/edit/${id}`, user)
+const deleteUser = (id) => axios.delete(`${API}/users/delete/${id}`)
+const resetPassword = (password, id) => axios.put(`${API}/users/resetpassword/${id}`, password)
 //AuthEndponts
 const Login = (credentials) => axios.post(`${API}/auth/login`, credentials)
 const Register = (credentials) => axios.post(`${API}/auth/register`, credentials)
 
-export { getProducts, getOrders, getUsers, Login, Register, addProduct, deleteProduct, editProduct }
+export {
+    Login,
+    Register,
+    getProducts,
+    getProductsCount,
+    addProduct,
+    editProduct,
+    deleteProduct,
+    getUsers,
+    getUsersCount,
+    addUser,
+    editUser,
+    deleteUser,
+    resetPassword,
+    getOrders,
+    addOrder,
+    getOrdersCount,
+    deleteOrder
+}
