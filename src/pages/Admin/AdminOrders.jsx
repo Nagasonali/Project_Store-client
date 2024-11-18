5a0b08cf4e4c00367eab45b476a493334270ec17
Sonali <nagasonali5676@gmail.com>
2024-11-18 15:24:51 +0530

    Day-19

--- src/pages/Admin/AdminOrders.jsx
+++ src/pages/Admin/AdminOrders.jsx
@@ -1,187 +1,109 @@
-import React, { useEffect, useState,useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminPageHeader from '../../components/Admin/AdminPageHeader'
-import { Loader2, Pencil, Trash, TriangleAlert,Plus,X } from 'lucide-react' 
-import { addOrders,getOrders,deleteOrder,editOrder} from '../../api/api'
+import { Loader2, Pencil, Trash, TriangleAlert } from 'lucide-react'
+import { deleteOrder, getOrders } from '../../api/api'
+import { toast } from 'sonner'
+
+
 const AdminOrders = () => {
+    const [orders, setOrders] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-const[orders,setOrders] = useState(null)
-const [loading, setLoading] = useState(true)
-const [currentOrder, setCurrentOrder] = useState(null)
-const [showAdd, setShowAdd] = useState(false);
-const [showEdit, setShowEdit] = useState(false)
-const userIDRef = useRef("");
-const productIDRef = useRef("");
-const totalRef = useRef(""); 
-const phoneRef = useRef(0);
-const address = useRef("");
-const orderAtRef = useRef("");
-async function fetchData(){
-  try {
-    const res = await getOrders()
-    if (res.status === 200) {
-      setOrders(res.data)
+    const fetchData = async () => {
+        try {
+            const res = await getOrders()
+            if (res.status === 200) {
+                setOrders(res.data)
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+        finally {
+            setLoading(false)
+        }
     }
 
-  } catch (error) {
-    console.log(error)
-  }
-  finally {
-    setLoading(false)
-  }
-}
+    const handleDelete = async (id) => {
+        try {
+            const response = await deleteOrder(id)
+            if (response.status === 200) {
+                // console.log("Product Deleted !")
 
-
-const editHelper = (order) => {
-  setCurrentOrder(order)
-  setShowEdit(true)
-
-}
-const handleEdit = async (e) => {
-  e.preventDefault()
-  const order = {
-    userID: userIDRef.current.value,
-    productID: productIDRef.current.value,
-    total: totalRef.current.value,
-    phone: phoneRef.current.value,
-    address: addressRef.current.value,
-    orderedAt: orderedAtRef.current.value
-  }
-  try {
-    const response = await editOrder(order, currentOrder._id)
-    if (response.status === 200) {
-      setShowEdit(!showEdit)
-      fetchData()
-      //info("Product Updated !")
+                toast.success('Order Deleted')
+                fetchData()
+            }
+        } catch (error) {
+            console.error(error)
+        }
     }
-  } catch (error) {
-   console. error("Error while Updating")
-  }
-}
-
-const handleDelete = async (id) => {
-  try {
-    const response = await deleteOrder(id);
-    if (response.status === 200) {
-      console.log("Order deleted");
-      fetchData();
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-
-useEffect(() => {
-  fetchData()
-}, [])
-
-if (loading) {
-  return (
-    <>
-      <div className='w-screen h-[90vh] flex flex-col justify-center items-center'>
-        <Loader2 className='text-purple-500 h-14 w-14 animate-spin' />
-      </div>
-    </>
-  )
-}
-if (!orders || orders.length === 0) {
-  return (
-    <>
-      <div className='w-screen h-[90vh] flex flex-col justify-center items-center'>
-        <TriangleAlert className='text-orange-400 h-12 w-12' />
-        <p>
-          No Orders Available !
-        </p>
-      </div>
-    </>
-  )
-}
-
-  return (
-    <div className='w-full h-full flex flex-col justify-start items-start'>
-      <div className="w-full flex flex-row justify-between items-center my-4 shadow-md rounded-md p-1 border">
-      <AdminPageHeader title='Orders'/>
-      <button
-          className="w-10 h-10 font-bold flex justify-center items-center border-2 border-green-500 rounded-md
-         text-green-500 shadow-md hover:text-white hover:bg-green-500 hover:shadow-md
-          hover:shadow-green-400"
-          onClick={() => setShowAdd(!showAdd)}
-        >
-          <Plus className="w-8 h-8" />
-        </button>
-      </div>
-      <table className='w-full h-5 border-collapse border shadow-lg rounded-md'>
-        <thead className='shadow-sm font-bold text-purple-500 text-left'>
-          <tr>
-            <th className='p-6'>UserID</th>
-            <th className='p-6'>ProductID</th>
-            <th className='p-6'>Total</th>
-            <th className='p-6'>Phone</th>
-            <th className='p-6'>Address</th>
-            <th className='p-6'>OrderedAt</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            orders.map((order,index)=>(
-
-           
-        <tr key={index} >
-                <td className='p-6'>{order.userID}</td>
-                <td className='p-6'>{order.productID}</td>
-                <td className='p-6'>{order.total}</td>
-                <td className='p-6'>{order.phone}</td>
-                <td className='p-6'>{order.address}</td>
-                <td className='p-6'>{order.orderedAt}</td>
-                <td className='p-6 flex h-full w-full flex-row justify-start items-center gap-4'>
-                  <button className='h-15 w-15 border-blue-500 border-2 p-1 rounded-md text-blue-500 shadow-md
-               hover:bg-blue-500 hover:text-white hover:shadow-blue-500'
-                  onClick={() => { editHelper(order) }} > <Pencil />
-                  </button>
-                  <button className='h-15 w-15 border-red-500 border-2 p-1 rounded-md text-red-500 shadow-md
-               hover:bg-red-500 hover:text-white hover:shadow-red-500'
-                                onClick={() => {
-                    handleDelete(order._id);
-                  }}
-                >        <Trash />
-                  </button>
-                </td>
-              </tr>
-               ))
-              }
-            
-          </tbody>
-          </table>    
-        
-
-{showEdit && (
-        <>
-          <div className="absolute top-0 left-0 z-50 h-screen w-screen flex justify-center items-center bg-black/40 ">
-            <div className='h-[55%] w-1/3 flex flex-col justify-center items-center bg-white shadow-2xl rounded-md'>
-              <div className='h-full w-full flex flex-col justify-center items-center text-lg font-semibold'>
-                <div className="h-[20%] w-[80%] flex flex-row justify-center items-center">
-                  <h1 className='w-1/2 text-left text-xl my-6 font-bold text-blue-500'>Edit Order</h1>
-                  <div className="w-1/2 flex justify-end items-center text-red-500 cursor-pointer" onClick={() => { setShowEdit(!showEdit) }}>
-                    <X className="h-8 w-8 border-2 p-1  border-red-500 rounded-full  hover:bg-red-500 hover:text-white" />
-                  </div>
+    useEffect(() => {
+        fetchData()
+    }, [])
+    // console.log(orders)
+    if (loading) {
+        return (
+            <>
+                <div className='w-screen h-[90vh] flex flex-col justify-center items-center'>
+                    <Loader2 className='text-purple-500 h-14 w-14 animate-spin' />
                 </div>
-                <form className='h-[70%] w-[80%] flex flex-col justify-center items-center gap-8' onSubmit={handleEdit}>
-                  <input ref={userIDRef} type="string" name="" id="UserID" placeholder='UserID' defaultValue={currentOrder.userID} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required autoFocus />
-                  <input ref={productIDRef} type="string" name="" id="productID" placeholder='ProductID' defaultValue={currentOrder.productID} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required />
-                  <input ref={total} type="string" name="" id="total" placeholder='Total' defaultValue={currentOrder.total} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required />
-                  <input ref={phone} type="number" name="" id="phone" placeholder='Phone' defaultValue={currentOrder.phone} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required />
-                  <input ref={address} type="string" name="" id="address" placeholder='Address' defaultValue={currentOrder.address} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required />
-                  <input ref={orderedAtRef} type="Date" name="" id="orderedAt" placeholder='OrderedAt' defaultValue={currentOrder.orderedAt} className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-blue-400 rounded-sm' required />
-                  <button type="submit" className="w-full h-[3rem]  shadow-lg shadow-gray-400 hover:shadow-blue-400 bg-blue-500 text-white rounded-sm outline-none">Save</button>
-                </form>
-              </div>
+            </>
+        )
+    }
+    if (!orders || orders.length === 0) {
+        return (
+            <>
+                <div className='w-screen h-[90vh] flex flex-col justify-center items-center'>
+                    <TriangleAlert className='text-orange-400 h-12 w-12' />
+                    <p>
+                        No Orders Available !
+                    </p>
+                </div>
+            </>
+        )
+    }
+    return (
+        <div className='w-full flex flex-col justify-start items-start'>
+            <div className='w-full flex flex-row justify-between items-center my-4 shadow-md rounded-md p-1 border'>
+                <AdminPageHeader title='Orders' />
             </div>
-          </div>
-        </>
-      )}
-    </div>
-  )
+            <table className='w-full h-full border-collapse border shadow-lg rounded-md'>
+                <thead className='shadow-md font-bold text-purple-500 text-left rounded-md'>
+                    <tr>
+                        <th className='p-6'>UID</th>
+                        <th className='p-6'>PID</th>
+                        <th className='p-6'>Phone</th>
+                        <th className='p-6'>Total</th>
+                        <th className='p-6'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        orders.map((order, index) => (
+                            <tr key={index}>
+                                <td className='p-4'>{order.uid}</td>
+                                <td className='p-4'>{order.pid}</td>
+                                <td className='p-4'>{order.phone} </td>
+                                <td className='p-4'>{order.total}</td>
+                                <td className='p-4 flex h-full w-full flex-row justify-start items-center gap-4'>
+                                    {/* <button className='h-15 w-15 border-blue-500 border-2 p-1 rounded-md text-blue-500 shadow-md
+               hover:bg-blue-500 hover:text-white hover:shadow-blue-500'>
+                                        <Pencil />
+                                    </button> */}
+                                    <button className='h-15 w-15 border-red-500 border-2 p-1 rounded-md text-red-500 shadow-md
+               hover:bg-red-500 hover:text-white hover:shadow-red-500'
+                                        onClick={() => { handleDelete(order._id) }}>
+                                        <Trash />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default AdminOrders
